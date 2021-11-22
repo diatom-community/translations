9597212865eb31fbe11919039ad8a1542e82a6c2
crud <crudiam@protonmail.com>
2021-11-23 00:02:54 +0100

    added form

--- src/pages/contributors.js
+++ src/pages/contributors.js
@@ -6,7 +6,17 @@ const ContributorsPage = (props) => {
   return (
     <main>
         <Layout pageTitle="Contributors" {...props}>
-            <p>TODO: explain how to contribute</p>
+
+            <script src="https://static.airtable.com/js/embed/embed_snippet_v1.js"></script>
+            <iframe 
+              className="airtable-embed airtable-dynamic-height" 
+              src="https://airtable.com/embed/shr98rE1fSj537F3n?backgroundColor=teal" 
+              frameBorder="0" 
+              onMouseWheel="" 
+              width="100%" 
+              height="1750" 
+              style={{background: "transparent"}}
+            ></iframe>
         </Layout>
     </main>
   )
